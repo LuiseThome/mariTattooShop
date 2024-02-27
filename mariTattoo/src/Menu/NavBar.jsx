398debcb9d766bbf3f1376476c91f029d942d72d
LuiseThome <luisethome@gmail.com>
2024-02-26 21:42:28 -0300

    [header] Fixing NavBar

--- mariTattoo/src/Menu/NavBar.jsx
+++ mariTattoo/src/Menu/NavBar.jsx
@@ -1,33 +1,60 @@
 import { Grid, Link, ListItem } from "@mui/material"
 
 
+
 export const NavBar = () => {
 
-    const itemsMenu = ["SOBRE", "ORÇAMENTO", "AGENDA"];
+    //const itemsMenu = ["SOBRE", "ORÇAMENTO", "AGENDA"];
+
+    const itemsMenu = [
+        {
+            to: "#sobre",
+            name: "SOBRE"
+        },
+        {
+            to: "#orçamento",
+            name: "ORÇAMENTO"
+        },
+        {
+            to: "#agenda",
+            name: "AGENDA"
+        }
+    ]
 
     return (
-        <Grid 
-        container 
-        wrap="nowrap"
+        <Grid
+        className="MenuGrid"
+        container
+        wrap="reverse-wrap"
         direction="row"
         alignItems="center"
-        justifyContent="center"
-        >    
-            {itemsMenu.map(item => (
+        justify="center"
+        sx={{
+            mx: "auto",
+            width: 325
+        }}
+        >
+                {itemsMenu.map(item => (
                 <ListItem
-                key={item}>
-                    <Link 
+                key={item}
+                >
+                    <Link
+                    className="MenuLink" 
+                    underline="none"
                     sx={{
-                    textDecoration: 'none',
-                    }}>
-                    {item}
+                    //color: 'white',
+                    }}
+                    href={item.to}
+                    >
+                    {item.name}
                     </Link>
                    
                 </ListItem>
                     
-            ))}
+            ))}  
+            
         </Grid>
     )
-   
 
 }
+
