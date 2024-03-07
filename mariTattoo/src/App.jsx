af2f7d4db6e3a66a2b379ed2d0f32ee2df0f61ba
LuiseThome <luisethome@gmail.com>
2024-03-07 20:58:32 -0300

    [bg-cover]Uploaded cover image

--- mariTattoo/src/App.jsx
+++ mariTattoo/src/App.jsx
@@ -1,4 +1,5 @@
 import { NavBar } from './Menu/NavBar'
+import { BgCover } from './Cover/BgCover'
 import "/App.css"
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
      <NavBar />
+     <BgCover />
     </>
   )
 }
---
+++ mariTattoo/src/Cover/BgCover.jsx
@@ -0,0 +1,11 @@
+import { CardMedia } from "@mui/material";
+
+export const BgCover = () => {
+    return (
+        <CardMedia
+        component="img"
+        image="Public/TATTOO SHOP copy.png"
+        alt="Foto de Capa"
+        />
+    )
+}
